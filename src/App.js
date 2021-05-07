14f2e4efdbc70cd52bb564fa6cf0c4163c626c1d
Abhishek Mohanty <abhishekm2106@gmail.com>
2021-05-07 19:54:55 +0530

    converted class components to functional components using hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import SearchBar from './components/search-bar/search-bar.component';
 import MusicList from './components/music-list/music-list.component';
 import './App.css';
 
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      token:'',
-      searchedItems:[]
-    }
-  }
+//   constructor(){
+//     super();
+//     this.state = {
+//       token:'',
+//       searchedItems:[]
+//     }
+//   }
 
-  updateItems = (newItems) => {
-      this.setState({searchedItems:newItems},()=>{console.log(this.state)})
-  }
+//   updateItems = (newItems) => {
+//       this.setState({searchedItems:newItems},()=>{console.log(this.state)})
+//   }
 
-  componentDidMount(){
-    const clientId = 'a0405fea39d946258aa3f328bc0e3a71';
-    const clientSecret = '0c68d04ade504af69422c9d52ae4480a';
+//   componentDidMount(){
+//    }
 
 
+
+
+//   render(){
+//     return (
+//       <div className="App">
+//         <div className="main-body">
+//           <SearchBar token={this.state.token} updateItems={this.updateItems}/>
+//           <MusicList items={this.state.searchedItems}/>
+//         </div>
+        
+//         <a href="https://www.linkedin.com/in/abhishekm2106/" target="_blank" rel="noopener noreferrer" className="credits">Abhishek Mohanty</a>
+//       </div>
+//     );
+//   }
+
+// }
+
+
+function App() {
+
+  const [token,setToken] = useState('')
+  const [items,changeItems] = useState([])
+  const clientId = 'a0405fea39d946258aa3f328bc0e3a71';
+  const clientSecret = '0c68d04ade504af69422c9d52ae4480a';
+
+  useEffect(() => {
     fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
@@ -30,22 +55,22 @@ class App extends React.Component {
             'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
         },
         body: 'grant_type=client_credentials'
-    }).then(response => response.json()).then(output => this.setState({token:output.access_token}))
-  }
+    }).then(response => response.json()).then(output => setToken(output.access_token))
+  },[])
 
-  render(){
-    return (
-      <div className="App">
-        <div className="main-body">
-          <SearchBar token={this.state.token} updateItems={this.updateItems}/>
-          <MusicList items={this.state.searchedItems}/>
-        </div>
-        
-        <a href="https://www.linkedin.com/in/abhishekm2106/" target="_blank" rel="noopener noreferrer" className="credits">Abhishek Mohanty</a>
+  return (
+    <div className='App'>
+      <div className='main-body'>
+        <SearchBar token={token} updateItems={changeItems}/>
+        <MusicList items={items}/>
       </div>
-    );
-  }
-
+      
+      <a href="https://www.linkedin.com/in/abhishekm2106/" target="_blank" rel="noopener noreferrer" className="credits">Abhishek Mohanty</a>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+
--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,50 +1,74 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './search-bar.style.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {search: ''};
+// class SearchBar extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {search: ''};
     
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//       }
     
-      handleChange(event) {
-        this.setState({search: event.target.value});
-      }
+//       handleChange(event) {
+//         this.setState({search: event.target.value},()=>{        
+//           this.handleSubmit();
+//         });
+//         console.log(window.innerWidth)
+
+//       }
     
-      handleSubmit(event) {
-        event.preventDefault();
-        var str = this.state.search
-        var replaced = str.replace(' ', '+');
+//       handleSubmit() {
+//         var str = this.state.search
+//         var replaced = str.replace(' ', '+');
+//             fetch(`https://api.spotify.com/v1/search?q=${replaced}&type=track`, {
+//               method: 'GET',
+//               headers: { 
+//                   'Accept': 'application/json',
+//                   'Content-Type': 'application/json',
+//                   'Authorization' : 'Bearer ' + this.props.token}
+//           }).then(response => response.json()).then(output => (output.tracks!==undefined) ? this.props.updateItems(output.tracks.items):this.props.updateItems([]));
+
+//       }
+    
+//       render() {
+//         return (
+//             <div className="search-bar">
+//                 <input className="input" placeholder="Search you Favourite Music" type="text" value={this.state.search} onChange={this.handleChange} />
+//                 <h2 className="title" style={{display : (this.state.search.length>0 && window.innerWidth<=920)? 'none': 'block'}}>Search In from the wide range of collections.</h2>
+//             </div>
+
+//         );
+//       }
+// }
+
+
+function SearchBar({token,updateItems}) {
+
+  const [input,changeInput] = useState('')
+
+  useEffect(()=>{
+        var replaced = input.replace(' ', '+');
+        
         fetch(`https://api.spotify.com/v1/search?q=${replaced}&type=track`, {
-            method: 'GET',
-            headers: { 
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization' : 'Bearer ' + this.props.token}
-        }).then(response => response.json()).then(output => this.props.updateItems(output.tracks.items) );
-    
-      }
-    
-      render() {
-        return (
+              method: 'GET',
+              headers: { 
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  'Authorization' : 'Bearer ' + token}
+          }).then(response => response.json())
+          .then(output => (output.tracks!==undefined) ? updateItems(output.tracks.items):updateItems([]));
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[input])
+
+  return (
             <div className="search-bar">
-                <form className="search-form" onSubmit={this.handleSubmit}>
-                    <label>
-                    <input className="input" placeholder="Search you Favourite Music" type="text" value={this.state.search} onChange={this.handleChange} />
-                    </label>
-                    <button className="submit-button" type="submit"><FontAwesomeIcon className="submit-icon"  icon={faSearch} /></button>
-                </form>
-
-                <h2 className="title">Search In from the wide range of collections.</h2>
+              <input className="input" placeholder="Search you Favourite Music" type="text" value={input} onChange={e=>changeInput(e.target.value)} />
+              <h2 className="title" style={{display : (input.length>0 && window.innerWidth<=920)? 'none': 'block'}}>Search In from the wide range of collections.</h2>
             </div>
-
-        );
-      }
+  )
 }
+
 
 export default SearchBar;
